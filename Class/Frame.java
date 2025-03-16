35dc677444a15077a78f4962b6ee7b75cb515dae
Karl Ferdinan <karlferdinan1@gmail.com>
2025-03-16 20:13:38 +0700

    Co-authored-by: Giliant Chandra <GiliantChandra@users.noreply.github.com>

--- Class/Frame.java
+++ Class/Frame.java
@@ -1,23 +1,33 @@
+import java.awt.Color;
+
 import javax.swing.*;
 
-public class Frame{
+public class Frame extends JPanel{
     private int tileSize = 32;
-    private int rows = 48;
+    private int rows = 24;
     private int columns = 32;
     private int width;
     private int height;
     JFrame frame = new JFrame("Ya main last war lah !!");
+
+    
+
 
 
     Frame(){
         width = columns * tileSize;
         height = rows * tileSize;
 
-        frame.setVisible(true);
         frame.setSize(width , height);
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        setBackground(Color.BLACK);
+        
+        frame.add(this);
+        
+        frame.setVisible(true);    
     }
 
     
